4304542899df598b20d1dbafe344808e9bd38d67
Michinobu Maeda <michinobumaeda@gmail.com>
2021-05-19 04:25:13 +0900

    bug fix

--- js/site.js
+++ js/site.js
@@ -6,9 +6,11 @@ function filterByTag(tag) {
     if ((item.nodeName || '').toUpperCase() !== 'LI') { continue; }
     item.style.display = (tag === 'ALL' || ` ${tags} `.includes(` ${tag} `)) ? 'list-item' : 'none'
   }
-  const tags = document.getElementById('tag-cloud').children
-  for (i = 0; i < tags.length; i++) {
-    tags[i].style.borderBottom = tags[i].id === `tag-${tag}` ? 'solid 2px #0000ff' : 'none'
+  if (document.getElementById('tag-cloud')) {
+    const tags = document.getElementById('tag-cloud').children
+    for (i = 0; i < tags.length; i++) {
+      tags[i].style.borderBottom = tags[i].id === `tag-${tag}` ? 'solid 2px #0000ff' : 'none'
+    }
+    document.getElementById('tag-ALL').style.display = tag === 'ALL' ? 'none' : 'inline'
   }
-  document.getElementById('tag-ALL').style.display = tag === 'ALL' ? 'none' : 'inline'
 }
