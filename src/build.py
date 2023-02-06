529372895d6ba8d35cb59c505ece939ce971d3d2
Michinobu Maeda <michinobumaeda@gmail.com>
2023-02-06 12:01:57 +0900

    refactoring

--- src/build.py
+++ src/build.py
@@ -8,51 +8,73 @@ import markdown
 from mdx_gfm import PartialGithubFlavoredMarkdownExtension
 
 
-def get_title(html):
-    ret = re.findall(
-        r"<h1(\s[^>]*)*>\s*(.*)\s*</h1>",
-        html,
-        flags=re.IGNORECASE
+class Category:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+class MdData:
+    def __init__(self, path):
+        self.path = path
+        self.md = open(self.path, encoding='utf8').read()
+        self.title = ''
+        self.updated_at = ''
+        self.tags = []
+
+        first_line = ''
+        second_line = ''
+        for line in self.md.split('\n'):
+            if first_line == '':
+                first_line = line
+            elif second_line == '':
+                second_line = line
+
+            # Get first '# '
+            if self.title == '':
+                ret = re.match(r"^#\s+(?P<text>.+)\s*$", line, re.IGNORECASE)
+                if ret and ret.group('text'):
+                    self.title = ret.group('text')
+
+            # Get first 'Update: '
+            if self.updated_at == '':
+                ret = re.match(r"^Update:\s*(?P<text>.+)\s*$",
+                               line, re.IGNORECASE)
+                if ret and ret.group('text'):
+                    self.updated_at = ret.group('text')
+
+            # Get last 'Tag: '
+            ret = re.match(r"^Tag:\s*(?P<text>.+)\s*$", line, re.IGNORECASE)
+            if ret:
+                self.tags = ret.group('text').lower().split()
+
+        if self.title == '' and re.match(r"^=+$", second_line):
+            self.title = first_line
+
+    def get_html(self):
+        return markdown.markdown(
+            self.md,
+            extensions=[PartialGithubFlavoredMarkdownExtension()]
+        ).replace(
+            'class="language-mermaid"',
+            'class="mermaid nohighlight"'
+        )
+
+    def get_html_path(self):
+        return re.sub(r"\.md$", '.html', self.path, flags=re.IGNORECASE)
+
+
+def save_html_from_md(tmpl, md, sample=False):
+    save_html(
+        tmpl,
+        md.get_html_path(),
+        md.get_html(),
+        md.title,
+        sample
     )
-    return ret[0][-1] if len(ret) else ''
 
 
-def get_updaed_at(html):
-    ret = re.findall(
-        r"<p(\s[^>]*)*>Update:\s*(.*)\s*</p>",
-        html,
-        flags=re.IGNORECASE
-    )
-    return ret[0][-1] if len(ret) else 'unknown'
-
-
-def get_tags(html):
-    ret = re.findall(
-        r"(<p(\s[^>]*)*>Tag:\s*(.*)\s*</p>)",
-        html,
-        flags=re.IGNORECASE
-    )
-    return ret[-1][-1].lower().split() if len(ret) else []
-
-
-def render_markdown(path):
-    return markdown.markdown(
-        open(
-            path,
-            encoding='utf8'
-        ).read(),
-        extensions=[PartialGithubFlavoredMarkdownExtension()]
-    ).replace(
-        'class="language-mermaid"',
-        'class="mermaid nohighlight"'
-    )
-
-
-def save_html(tmpl, path, content, sample=False):
-    title = get_title(content)
-    updated_at = get_updaed_at(content)
-    tags = get_tags(content)
-
+def save_html(tmpl, path, content, title, sample=False):
     open(
         path,
         'w',
@@ -70,12 +92,6 @@ def save_html(tmpl, path, content, sample=False):
         )
     )
 
-    return (title, updated_at, tags)
-
-
-def sort_key_path(el):
-    return el['path']
-
 
 def sort_key_updated_at(el):
     ret = re.match(r"^2.*$", el['updated_at'])
@@ -84,12 +100,7 @@ def sort_key_updated_at(el):
     return f'{ts} {path}'
 
 
-def generate_cat_index(pages, cat, title):
-    pages.sort(key=sort_key_updated_at, reverse=True)
-
-    html = f'<h1>{title}</h1>\n'
-
-    html = f'{html}<ul id="tag-cloud">\n'
+def generate_tag_cloud_data(pages, cat):
     cloud = {}
     for page in pages:
         if page['cat'] == cat:
@@ -101,18 +112,34 @@ def generate_cat_index(pages, cat, title):
                     cloud[tag] = 1
     tag_names = list(cloud.keys())
     tag_names.sort()
-    for name in tag_names:
-        size = min(math.ceil(math.log(cloud[name], 2)) + 1, 9)
-        html = f'{html}<li id="tag-{name}"><a href="/{cat}/?tag={name}" class="tag-{size}">{name}</a></li>'
-    html = f'{html}</ul>\n'
 
-    if len(tag_names):
+    ret = []
+    for name in tag_names:
+       ret.append({
+            'name': name,
+            'size': min(math.ceil(math.log(cloud[name], 2)) + 1, 9),
+        })
+    return ret
+
+
+def generate_cat_index(pages, cat, title):
+    html = f'<h1>{title}</h1>\n'
+
+    tag_cloud = generate_tag_cloud_data(pages, cat)
+
+    if tag_cloud:
+        html = f'{html}<ul id="tag-cloud">\n'
+        for tag in tag_cloud:
+            name = tag['name']
+            size = tag['size']
+            html = f'{html}<li id="tag-{name}"><a href="/{cat}/?tag={name}" class="tag-{size}">{name}</a></li>'
+        html = f'{html}</ul>\n'
         html = f'{html}<p id="tag-ALL"><a href="/{cat}/">フィルタ解除</a></p>\n'
 
     html = f'{html}<ul id="toc">\n'
     for page in pages:
         if page['cat'] == cat:
-            updated_at = page['updated_at']
+            updated_at = page['updated_at'] if page['updated_at'] else 'unknown'
             title = page['title']
             path = page['path']
             tags = ' '.join(page['tags'])
@@ -124,15 +151,14 @@ def generate_cat_index(pages, cat, title):
 if __name__ == '__main__':
     src = os.path.dirname(__file__)
     docs = os.path.join(src, '..', 'docs')
-    categories = {
-        'l': '厚生部',
-        'p': '政治局',
-        't': '工作室',
-    }
     meta_path = os.path.join(docs, 'meta.json')
-    ts = datetime.datetime.now(
-        pytz.timezone('Asia/Tokyo')
-    ).isoformat()
+    ts = datetime.datetime.now(pytz.timezone('Asia/Tokyo')).isoformat()
+
+    categories = [
+        Category('t', '工作室'),
+        Category('l', '厚生部'),
+        Category('p', '政治局'),
+    ]
 
     tmpl = open(
         os.path.join(src, 'template.html'),
@@ -144,52 +170,41 @@ if __name__ == '__main__':
 
     pages = []
 
-    md_path = os.path.join(src, 'sample.md')
-    save_html(
+    save_html_from_md(
         tmpl,
-        re.sub(r"\.md$", '.html', md_path, flags=re.IGNORECASE),
-        render_markdown(md_path),
+        MdData(os.path.join(src, 'sample.md')),
         sample=True
     )
 
-    md_path = os.path.join(docs, 'index.md')
-    save_html(
+    save_html_from_md(
         tmpl,
-        re.sub(r"\.md$", '.html', md_path, flags=re.IGNORECASE),
-        render_markdown(md_path),
+        MdData(os.path.join(docs, 'index.md')),
     )
 
-    for cat in categories.keys():
-        for root, dirs, files in os.walk(os.path.join(docs, cat)):
+    for cat in categories:
+        for root, dirs, files in os.walk(os.path.join(docs, cat.id)):
             for file in files:
                 if re.match(r".*\.md$", file, flags=re.IGNORECASE):
-                    md_path = os.path.join(root, file)
-                    (title, updated_at, tags) = save_html(
-                        tmpl,
-                        re.sub(r"\.md$", '.html', md_path,
-                               flags=re.IGNORECASE),
-                        render_markdown(md_path),
-                    )
-
-                    dir = os.path.relpath(root, docs).replace('\\', '/')
+                    md = MdData(os.path.join(root, file))
+                    save_html_from_md(tmpl, md)
                     pages.append({
-                        'cat': cat,
-                        'path': re.sub(r"\.md$", '.html', f'{dir}/{file}', flags=re.IGNORECASE),
-                        'title': title if title else 'unknown',
-                        'updated_at': updated_at,
-                        'tags': tags,
+                        'cat': cat.id,
+                        'path': os.path.relpath(md.get_html_path(), docs).replace('\\', '/'),
+                        'title': md.title,
+                        'updated_at': md.updated_at,
+                        'tags': md.tags,
                     })
 
-    for cat in categories.keys():
+    pages.sort(key=sort_key_updated_at, reverse=True)
+
+    for cat in categories:
         save_html(
             tmpl,
-            os.path.join(docs, cat, 'index.html'),
-            generate_cat_index(pages, cat, categories[cat]),
+            os.path.join(docs, cat.id, 'index.html'),
+            generate_cat_index(pages, cat.id, cat.name),
+            cat.name
         )
 
-    pages.sort(key=sort_key_path)
-    meta = {
-        'pages': pages
-    }
+    # pages.sort(key=sort_key_path)
     with open(meta_path, 'w', encoding='utf-8') as f:
-        json.dump(meta, f, ensure_ascii=False, indent=4)
+        json.dump({'pages': pages}, f, ensure_ascii=False, indent=4)
